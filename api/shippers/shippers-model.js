c1f4c3e2375112c5e7599a89737bb59ea9351b92
Josh Rime <joshrime1990@icloud.com>
2025-01-18 16:41:46 -0600

    get,byid,update model working

--- api/shippers/shippers-model.js
+++ api/shippers/shippers-model.js
@@ -1,27 +1,38 @@
+const db = require("../../data/db-config");
+
 module.exports = {
   get,
   getById,
   create,
   update,
   remove,
-}
+};
 
 async function get() {
-  return 'get wired'
+  // const result = await db.raw('select * from shippers')
+  const result = await db("shippers");
+  return result;
 }
 
-async function getById() {
-  return 'getById wired'
+async function getById(shipperId) {
+  const [shipper] = await db("shippers").where("shipperid", shipperId);
+  return shipper;
 }
 
-async function create() {
-  return 'create wired'
+async function create(shipper) {
+  const [shipperId] = await db("shippers").insert(shipper);
+  // console.log(shipperId);
+  const result = await getById(shipperId);
+  // console.log(result);
+  return result;
 }
 
-async function update() {
-  return 'update wired'
+async function update(shipperId, changes) {
+  await db("shippers").update(changes).where("shipperid", shipperId);
+  const result = await getById(shipperId);
+  return result;
 }
 
 async function remove() {
-  return 'delete wired'
+  return "delete wired";
 }
